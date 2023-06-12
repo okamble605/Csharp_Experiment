55db1b54d994a45e9fe85fa9ffc47091b6cb4970
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2023-06-12 20:38:00 +0530

    Update regx.cs

--- Experiment/Exp10/regx.cs
+++ Experiment/Exp10/regx.cs
@@ -1,17 +1,29 @@
 using System;
 using System.Text.RegularExpressions;
 
-namespace Examples
+class Program
 {
-    public class Example2
+    static void Main()
     {
-        public static void Main()
+        string input = "Hello, my email is omkamble121@gmail.com and my phone number is (123) 456-7890.";
+        string emailPattern = @"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b";
+        string phonePattern = @"\(\d{3}\)\s*\d{3}-\d{4}";
+        Regex emailRegex = new Regex(emailPattern);
+        Regex phoneRegex = new Regex(phonePattern);
+
+        MatchCollection emailMatches = emailRegex.Matches(input);
+        Console.WriteLine("Email Matches:");
+        foreach (Match match in emailMatches)
         {
-            string pattern = @"\ba\w*\b";
-            string input = "An extraordinary day dawns with each new day.";
-            Match m = Regex.Match(input, pattern, RegexOptions.IgnoreCase);
-            if (m.Success)
-                Console.WriteLine("Found '{0}' at position {1}.", m.Value, m.Index);
+            Console.WriteLine(match.Value);
+        }
+
+        Console.WriteLine();
+        MatchCollection phoneMatches = phoneRegex.Matches(input);
+        Console.WriteLine("Phone Number Matches:");
+        foreach (Match match in phoneMatches)
+        {
+            Console.WriteLine(match.Value);
         }
     }
 }
